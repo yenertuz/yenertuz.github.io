aeaaa552e512d8ea32ab0e5901175b187e75cb09
unknown <yenertuz@hotmail.com>
2018-07-27 22:54:59 -0700

    m

--- update.php
+++ update.php
@@ -24,6 +24,10 @@ else if ($argv[1] == "log")
 {
 	log_project($json, $argv[2], $argv[3]);
 }
+else if ($argv[1] == "delete")
+{
+	unset($json["projects"][$argv[2]]);
+}
 else
 {
 	$json["return"] = "ERROR: ".$argv[1]." IS NOT A VALID COMMAND";

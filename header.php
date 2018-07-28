c8196e4b54b18d8c2a6728e903340c32c9c5ed73
unknown <yenertuz@hotmail.com>
2018-07-27 22:38:13 -0700

    m

---
+++ header.php
@@ -0,0 +1,57 @@
+<?php
+
+function	get_timestamp()
+{
+	date_default_timezone_get("America/Los_Angeles");
+	return (date('d.m.Y, H:i:s'));
+}
+
+function	set_project($json, $project_name, $project_status)
+{
+	if ($project_status != "assigned" && $project_status != "working"
+			&& $project_status != "review" && $project_status != "finished")
+	{
+		$json["return"] = "ERROR: ".$project_status." IS AN INVALID PROJECT STATUS";
+		return ($json);
+	}
+	$json["projects"][$project_name]["status"] = $project_status;
+	$json["projects"][$project_name]["timestamp"] = get_timestamp();
+	return ($json);
+}
+
+function	log_project($json, $project_name, $description)
+{
+	if (isset($json["projects"][$project_name]) == 0)
+	{
+		$json["return"] = "ERROR: ".$project_name." NOT A PROJECT";
+		return($json);
+	}
+	$json["projects"][$project_name]["timestamp"] = get_timestamp();
+	$json["projects"][$project_name]["log"][] = $description;
+	return ($json);
+}
+
+function datediffInWeeks($date1, $date2)
+{
+    if($date1 > $date2)
+	{
+		return (datediffInWeeks($date2, $date1));
+	}
+    $first = DateTime::createFromFormat('m/d/Y', $date1);
+    $second = DateTime::createFromFormat('m/d/Y', $date2);
+    return (floor($first->diff($second)->days/7));
+}
+
+function	erase_old_projects($json)
+{
+	foreach ($json["projects"] as $key=>$value)
+	{
+		if (datediffInWeeks($json["projects"][$key]["timestamp"], get_timestamp()) != 0)
+		{
+			unset($json["projects"][$key]);
+		}
+	}
+	return ($json);
+}
+
+?>
--- update.php
+++ update.php
@@ -1,20 +1,42 @@
 <?php
 
+include("header.php");
+
 system("git pull -f");
 
-$json = json_decode(file_get_contents("json"));
+$json = json_decode(file_get_contents("json"), true);
 
-date_default_timezone_set("America/Los_Angeles");
-$tmp = date('d.m.Y, H:i:s');
+$json["timestamp"] = get_timestamp();
 
-$json["last_update"] = $tmp;
+$json["return"] = "OK";
 
 //print_r($json);
+
+if ($argv[1] == "needwork")
+{
+	$json["need_work"] = get_timestamp();
+}
+else if ($argv[1] == "set")
+{
+	set_project($json, $argv[2], $argv[3]);
+}
+else if ($argv[1] == "log")
+{
+	log_project($json, $argv[2], $argv[3]);
+}
+else
+{
+	$json["return"] = "ERROR: ".$argv[1]." IS NOT A VALID COMMAND";
+}
+
+$json = erase_old_projects($json);
 
 file_put_contents("json", json_encode($json));
 
 system("git add .");
 system("git commit -m \"m\"");
 system("git push");
+
+echo($json["return"].PHP_EOL);
 
 ?>

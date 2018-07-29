27a462b37dbeece5c654c679414ab3877529c972
unknown <yenertuz@hotmail.com>
2018-07-29 00:09:07 -0700

    m

--- header.php
+++ header.php
@@ -9,7 +9,7 @@ function	write_to_index_html($json)
 		$to_write .= "<div id=\"need_work\">NEED WORK!</div><br>\n";
 	}
 	$to_write .= "<div id=\"last_updated\">LAST UPDATED: ".$json["timestamp"]."</div><br>\n";
-	$to_write .= "<table>\n";
+	$to_write .= "<table width=\"100%\">\n";
 	$to_write .= "<tr id=\"columns\"><th>STATUS</th><th>NAME</th><th>LAST UPDATE</th></tr>\n";
 	foreach ($json["projects"] as $key=>$value)
 	{

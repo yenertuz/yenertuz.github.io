47ec633b4706d4aa78b4bddf7dd30acadfddcd27
yenertuz <yenertuz@hotmail.com>
2019-03-03 23:13:59 -0800

    create development

---
+++ env.js
@@ -0,0 +1,15 @@
+const environment = {
+	projects: [
+		{
+			"title": "Project 1",
+			"description": "React, Laravel",
+			"source": "www.google.com"
+		},
+
+		{
+			"title": "Project 2",
+			"description": "React, JavaScript",
+			"source": "www.facebook.com"
+		}
+	]
+};
